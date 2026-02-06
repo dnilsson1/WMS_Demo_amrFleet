a4f1a8a6be1833451de025c60408d48345d6f3d3
Daniel <daniel.nilsson111@gmail.com>
2026-02-06 12:30:35 +0100

    updated vit config

--- wms-demo/frontend/API-Demo/vite.config.js
+++ wms-demo/frontend/API-Demo/vite.config.js
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		host: true,
+		port: 5173,
+		strictPort: true,
+		hmr: {
+			clientPort: 5173
+		},
 		proxy: {
 			'/api': {
 				target: 'http://backend:8000',
